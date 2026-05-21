e82581b50e2b1bf95937f7b5f770891427327c29
EugeneLYH <eugeneloh19@gmail.com>
2026-05-21 16:59:18 +0800

    c9

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,14 +9,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router';
 import { router } from './app/router/Routes.tsx'
+import { store, StoreContext } from './lib/stores/store.ts';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StoreContext.Provider value={store}>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
-    </QueryClientProvider>
+    </QueryClientProvider>      
+    </StoreContext.Provider>
+
   </StrictMode>,
 )
